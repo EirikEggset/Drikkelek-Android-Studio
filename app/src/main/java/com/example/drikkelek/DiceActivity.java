67825233899ad0c4d3a222da196be438a6602b95
EirikEggset <78444318+EirikEggset@users.noreply.github.com>
2021-04-08 18:44:27 +0200

    Started on 3D-Object implementation

--- app/src/main/java/com/example/drikkelek/DiceActivity.java
+++ app/src/main/java/com/example/drikkelek/DiceActivity.java
@@ -10,30 +10,15 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class DiceActivity extends AppCompatActivity {
 
-    private Button diceBtn;
-    private Button btnCloseGame;
+    private DiceGLSurfaceView diceView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dice);
 
-        diceBtn = (Button) findViewById(R.id.dice_btn);
-        btnCloseGame = (Button) findViewById(R.id.btn_close_game);
+        diceView = new DiceGLSurfaceView(this);
+        setContentView(diceView);
 
-        diceBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int randomNumber = ThreadLocalRandom.current().nextInt(1, 7);
-                diceBtn.setText(randomNumber + "");
-            }
-        });
 
-        btnCloseGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
 
     }
 }
